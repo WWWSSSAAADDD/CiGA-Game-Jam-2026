57932dffc87ff02e4cbb869571de522ce10214e8
WWWSSSAAADDD <3781375775@qq.com>
2026-07-04 12:26:40 +0800

    feat：新增共享资源类型枚举 ResourceType
    小行星模块的资源类型和背包模块的资源 key 共用的枚举，纯数据无行为。

---
+++ Assets/Scripts/Infrastructure/ResourceType.cs
@@ -0,0 +1,13 @@
+namespace Game.Infrastructure
+{
+    /// <summary>
+    /// 资源种类。小行星粉碎后产出的资源类型，同时也是背包模块存资源用的 key。
+    /// 纯数据、不含任何行为，小行星模块和背包模块都直接复用这一个枚举。
+    /// </summary>
+    public enum ResourceType
+    {
+        Metal,
+        Ice,
+        Crystal
+    }
+}
